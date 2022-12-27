fdde0073dbc23835653f96555c86126e37054625
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-12-27 17:15:20 +0700

    Seperate Modal

--- app/Views/item/index.php
+++ app/Views/item/index.php
@@ -37,26 +37,7 @@
     </table>
 </div>
 
-<!-- Modal Create-->
-<div class="modal fade" id="modalCreate" tabindex="-1" aria-labelledby="modalCreateLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="modalCreateLabel">Create Item</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <div class="mb-3">
-            <label for="item_name" class="form-label">Nama Item</label>
-            <input type="text" class="form-control" id="item_name" placeholder="Item Pertama">
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+<!-- Adding Modals -->
+<?= $this->include('item/modals/create') ?>
 
 <?= $this->endSection() ?>
