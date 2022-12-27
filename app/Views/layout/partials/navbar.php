8994a3a66a0ce4b3e474ebf569cd3e3871447eeb
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-12-27 09:46:55 +0700

    Update on Navbar

--- app/Views/layout/partials/navbar.php
+++ app/Views/layout/partials/navbar.php
@@ -12,5 +12,8 @@
       <li class="nav-item">
         <a class="nav-link text-light" href="#">About</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link text-light" href="#">Items</a>
+      </li>
   </div>
 </nav>
